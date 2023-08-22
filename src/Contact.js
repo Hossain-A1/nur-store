ebdc2d77ed7bd9f8dcfcc87558d602eb85f4d338
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-08-22 18:54:19 +0300

    error page added

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ const ContactPage = () => {
   return (
     <section className='wrapper section-p flex flex-col items-center justify-start gap-10'>
       <h2 className="text-xl font-medium text-center capitalize text-slate-900">feel free to contact us</h2>
-      <div className='w-full h-[60vh] mb-20'>
+      <div className='w-[100vw] h-[60vh] mb-20'>
         <iframe
           src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3572.8727791712267!2d50.094617437010335!3d26.42758178087177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49fc83df937721%3A0x7d3b80f43597ec51!2sLuLu%20Mall%20-%20Dammam!5e0!3m2!1sen!2ssa!4v1692687058701!5m2!1sen!2ssa'
           width='600'
--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import Button from "./components/Button"
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section className='min-h-screen wrapper section-p'>
+ <div className="">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h2 className="text-4xl font-medium">404</h2>
+          <h3>UH OH! You're lost.</h3>
+          <p className="w-2/3">
+            The page you are looking for does not exist. How you got here is a
+            mystery. But you can click the button below to go back to the
+            homepage.
+          </p>
+
+         <Button to="/" color="violet_btn" placeholder="Go back to home" />
+        </div>
+      </div>
+
+    </section>
   )
 }
 
